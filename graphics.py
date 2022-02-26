1618a52e6b0744bf152cbadb328390542fe42c1e
hhw1101 <jenniferhuang2002@gmail.com>
2022-02-26 21:23:50 +0000

    tile display

--- graphics.py
+++ graphics.py
@@ -1,15 +1,29 @@
-
+# import libraries
 import utils
 import pygame
 
+# Aspect ratio
+# aspect ratio that was ideal was 600 to 45 and 800 to 35
+image_ratio_w = 40.0/800
+image_ratio_h = 50.0/600
+
+# images
+# Tiles
+# Tile naming convention
+""" For the numbered suits the image is named with a number then the letter of the suit """
+""" For the winds and dragons the image is saved a singular letter """
+
+# unused tiles generation and display
 tile_backing = pygame.image.load('./mahjong-tiles/back.jpg')
-tile_width,tile_height = tile_backing.get_size()
-tile_backing = pygame.transform.scale(tile_backing,(tile_width,tile_height))
+tile_width= image_ratio_w*SCREEN_WIDTH
+tile_height = image_ratio_h*SCREEN_HEIGHT
+tile_backing = pygame.transform.scale(tile_backing,(tile_width*image_ratio_w,tile_height*image_ratio_h))
 for i in range(18):
     screen.blit(tile_backing,(100+i*tile_width,100))
 
 pygame.display.update()
-# Tile print
+
+# Player 1 tile display
 # not the best
 for i in range(13):
     tiles = players[0][i]
