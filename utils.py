9d67e92d707016a60128f048b67ff4921b6e8b5c
WoonSim Tan <tan.woonsim@outlook.com>
2022-02-27 07:23:52 +0000

    add check fo jihu (that has bugs)

--- utils.py
+++ utils.py
@@ -1,22 +1,23 @@
 import random
 import pygame
-from pygame.locals import (K_ESCAPE, KEYDOWN, QUIT)
+from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
+
 
 def create_screen():
-# Create the screen object
-# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+    # Create the screen object
+    # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    GREEN = (0 ,105, 53)
+    GREEN = (0, 105, 53)
     WHITE = (255, 255, 255)
     screen.fill(GREEN)
     pygame.display.update()
 
     # Setting up caption
     pygame.display.set_caption("Mahjong Game")
-    
+
     # Loading image for the icon
-    icon = pygame.image.load('icon.jpg')
-    
+    icon = pygame.image.load("icon.jpg")
+
     # Setting the game icon
     pygame.display.set_icon(icon)
 
@@ -29,19 +30,20 @@ def create_screen():
     gong_button = large_font.render("杠", True, WHITE)
     chi_button = large_font.render("吃", True, WHITE)
     hu_button = large_font.render("胡", True, WHITE)
-    
+
     # Gets_rectangular covering of text
     peng_button_rect = peng_button.get_rect()
     gong_button_rect = gong_button.get_rect()
     chi_button_rect = chi_button.get_rect()
     hu_button_rect = hu_button.get_rect()
-    
+
     # Places the text
     peng_button_rect.center = (280, 400)
     gong_button_rect.center = (360, 400)
     chi_button_rect.center = (440, 400)
     hu_button_rect.center = (520, 400)
     return screen
+
 
 # Card class definition
 class Tile:
@@ -54,6 +56,7 @@ class Tile:
 
     def __lt__(self, other):
         return self.suit_type < other.suit_type
+
 
 def create_tiles(suit_values):
     all_tiles = []
@@ -70,19 +73,21 @@ def create_tiles(suit_values):
     random.shuffle(all_tiles)
     return all_tiles, []
 
+
 def distribute_tiles(all_tiles):
-    players = [[],[],[],[]]
+    players = [[], [], [], []]
     # distribute tiles to players
-    for i in range (3):
+    for i in range(3):
         for j in range(4):
             for k in range(4):
-                players[j].append(all_tiles[j*4 + k])
+                players[j].append(all_tiles[j * 4 + k])
         all_tiles = all_tiles[16:]
     for i in range(4):
         players[i].append(all_tiles[i])
         players[i] = sort_tiles(players[i])
     all_tiles = all_tiles[4:]
     return players, all_tiles
+
 
 def print_tiles(tiles_list):
     tiles_to_print = []
@@ -91,20 +96,31 @@ def print_tiles(tiles_list):
         tiles_to_print.append(tile.suit_type + tile.value)
     print(tiles_to_print)
 
+
 def sort_tiles(player_tiles):
-    tiles_sorted = sorted(player_tiles, key = lambda x: (x.suit_type, x.value), reverse = False)
+    tiles_sorted = sorted(
+        player_tiles, key=lambda x: (x.suit_type, x.value), reverse=False
+    )
     return tiles_sorted
+
 
 def check_for_chi(player_tiles, discarded_tile):
     wanted_tiles = []
     for i in range(5):
-        wanted_tiles.append(Tile(discarded_tile.suit_type, str(int(discarded_tile.value) - 2 + i)))
-    new_tile_set = player_tiles
+        wanted_tiles.append(
+            Tile(discarded_tile.suit_type, str(int(discarded_tile.value) - 2 + i))
+        )
+    new_tile_set = player_tiles.copy()
     new_tile_set.append(discarded_tile)
     for i in range(3):
-        if (wanted_tiles[i] in new_tile_set) and (wanted_tiles[i+1] in new_tile_set) and (wanted_tiles[i+2] in new_tile_set):
+        if (
+            (wanted_tiles[i] in new_tile_set)
+            and (wanted_tiles[i + 1] in new_tile_set)
+            and (wanted_tiles[i + 2] in new_tile_set)
+        ):
             return True
     return False
+
 
 def check_for_peng(player_tiles, discarded_tile):
     count = 0
@@ -116,6 +132,7 @@ def check_for_peng(player_tiles, discarded_tile):
     else:
         return False
 
+
 def check_for_gong(player_tiles, discarded_tile):
     count = 0
     for i in range(len(player_tiles)):
@@ -126,19 +143,119 @@ def check_for_gong(player_tiles, discarded_tile):
     else:
         return False
 
+
 def check_for_win(player_tiles):
     ping_hu = check_for_pinghu(player_tiles)
     peng_peng_hu = check_for_pengpenghu(player_tiles)
-    ji_hu = False
-    ban_se = False
-    qing_yi_se = False
+    ji_hu = check_for_jihu(player_tiles)
     shisan_yao = False
-    #flower_hu = False
-    win = ping_hu or peng_peng_hu or ji_hu or ban_se or qing_yi_se or shisan_yao #or flower_hu
+    # flower_hu = False
+    win = ping_hu or peng_peng_hu or ji_hu or shisan_yao  # or flower_hu
     return win
+
+
+def check_for_seqence(tile_list):
+    # if sequence exists remove it
+    for tile1 in tile_list:
+        if tile_list.count(tile1) == 1:
+            tile2 = Tile(tile1.suit_type, str(int(tile1.value) + 1))
+            tile3 = Tile(tile1.suit_type, str(int(tile1.value) + 2))
+        if tile1 in tile_list and tile2 in tile_list and tile3 in tile_list:
+            tile_list.remove(tile1)
+            tile_list.remove(tile2)
+            tile_list.remove(tile3)
+    for tile1 in reversed(tile_list):
+        if tile_list.count(tile1) == 1:
+            tile2 = Tile(tile1.suit_type, str(int(tile1.value) - 1))
+            tile3 = Tile(tile1.suit_type, str(int(tile1.value) - 2))
+        if tile1 in tile_list and tile2 in tile_list and tile3 in tile_list:
+            tile_list.remove(tile1)
+            tile_list.remove(tile2)
+            tile_list.remove(tile3)
+    return True
+
+
+def check_for_jihu(player_tiles):
+    # TODO: Fix this
+    tiles_to_check = player_tiles.copy()
+    tiles_set = []
+    occurances = []
+    for tile in tiles_to_check:
+        if tile not in tiles_set:
+            tiles_set.append(tile)
+            occurances.append(1)
+        else:
+            occurances[tiles_set.index(tile)] += 1
+    for i in range(len(occurances)):
+        if (
+            occurances[i] == 4
+            or (tiles_set[i].suit_type == "Dragon" and occurances[i] == 3)
+            or (tiles_set[i].suit_type == "Wind" and occurances[i] == 3)
+        ):
+            for i in range(3):
+                tiles_to_check.remove(tiles_set[i])
+    triples = sum(1 for v in occurances if v == 0)
+    if triples == 0:
+        # check for all seq
+        if check_for_pinghu(tiles_to_check):
+            return True
+    if triples == 1:
+        # remove peng and check that remaining sequences
+        tile = tiles_set[occurances.index(3)]
+        for i in range(3):
+            tiles_to_check.remove(tile)
+        if check_for_pinghu(tiles_to_check):
+            return True
+    if triples == 2:
+        #
+        if len(tiles_to_check) == 8:
+            # find sequence
+            for tile in tiles_set:
+                wanted_tiles = []
+                for i in range(5):
+                    wanted_tiles.append(
+                        Tile(tile.suit_type, str(int(tile.value) - 2 + i))
+                    )
+                for i in range(3):
+                    if (
+                        (wanted_tiles[i] in tiles_to_check)
+                        and (wanted_tiles[i + 1] in tiles_to_check)
+                        and (wanted_tiles[i + 2] in tiles_to_check)
+                    ):
+                        tiles_to_check.remove(wanted_tiles[i])
+                        tiles_to_check.remove(wanted_tiles[i + 1])
+                        tiles_to_check.remove(wanted_tiles[i + 2])
+            # check remaining all triples
+            if check_for_pengpenghu(tiles_to_check):
+                return True
+        # elif len(tiles_to_check) == 11:
+
+        # elif len(tiles_to_check) == 14:
+
+    if triples == 3:
+        # find sequence
+        for tile in tiles_set:
+            wanted_tiles = []
+            for i in range(5):
+                wanted_tiles.append(Tile(tile.suit_type, str(int(tile.value) - 2 + i)))
+            for i in range(3):
+                if (
+                    (wanted_tiles[i] in tiles_to_check)
+                    and (wanted_tiles[i + 1] in tiles_to_check)
+                    and (wanted_tiles[i + 2] in tiles_to_check)
+                ):
+                    tiles_to_check.remove(wanted_tiles[i])
+                    tiles_to_check.remove(wanted_tiles[i + 1])
+                    tiles_to_check.remove(wanted_tiles[i + 2])
+        # check remaining all triples
+        if check_for_pengpenghu(tiles_to_check):
+            return True
+    return False
+
 
 def check_for_pair(tile1, tile2):
     return tile1 == tile2
+
 
 def check_for_pinghu(player_tiles):
     tiles_to_check = player_tiles.copy()
@@ -151,14 +268,19 @@ def check_for_pinghu(player_tiles):
                 return False
         else:
             tile1 = tiles_to_check[0]
-            tile2 = Tile(tile1.suit_type, str(int(tile1.value)+1))
-            tile3 = Tile(tile1.suit_type, str(int(tile1.value)+2))
-            if tile1 in tiles_to_check and tile2 in tiles_to_check and tile3 in tiles_to_check:
+            tile2 = Tile(tile1.suit_type, str(int(tile1.value) + 1))
+            tile3 = Tile(tile1.suit_type, str(int(tile1.value) + 2))
+            if (
+                tile1 in tiles_to_check
+                and tile2 in tiles_to_check
+                and tile3 in tiles_to_check
+            ):
                 tiles_to_check.remove(tile1)
                 tiles_to_check.remove(tile2)
                 tiles_to_check.remove(tile3)
         count += 1
     return False
+
 
 def check_for_pengpenghu(player_tiles):
     tiles_to_check = player_tiles.copy()
@@ -176,6 +298,7 @@ def check_for_pengpenghu(player_tiles):
                     tiles_to_check.remove(tile)
         count += 1
     return False
+
 
 def check_for_yaojiu(player_tiles):
     if check_for_pengpenghu(player_tiles):
