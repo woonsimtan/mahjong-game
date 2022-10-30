8cbabde2488c446dcdf3f671073a6988fcc66304
hhw1101 <jenniferhuang2002@gmail.com>
2022-10-30 06:31:31 +0000

    testing check for win and edits that seem to cover more casesW

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from math import fabs
+
 import random
 
 class Tile:
@@ -43,8 +43,8 @@ def create_tiles():
     # random.shuffle(all_tiles)
     # return list of tiles
     return all_tiles
+    
 def check_for_win(tile_list):
-
     tile_set = []
     for tile in tile_list:
         if tile not in tile_set:
@@ -63,8 +63,6 @@ def check_for_win(tile_list):
         while len(test_list) > 0:
             tile = test_list[0]
             if test_list.count(tile) < 3:
-                if isinstance(tile.value, str):
-                    return
                 tile2 = Tile(tile.suit_type, str(int(tile.value) + 1))
                 tile3 = Tile(tile.suit_type, str(int(tile.value) + 2))
                 if tile2 in test_list and tile3 in test_list:
@@ -81,14 +79,36 @@ def check_for_win(tile_list):
 
     return False
 
+def check_for_sets_of_three(test_list):
+
+    while len(test_list) > 0:
+        tile = test_list[0]
+        if test_list.count(tile) < 3 and isinstance(tile.value, int):
+            tile2 = Tile(tile.suit_type, (tile.value) + 1)
+            tile3 = Tile(tile.suit_type, (tile.value) + 2)
+            if tile2 in test_list and tile3 in test_list:
+                test_list.remove(tile)
+                test_list.remove(tile2)
+                test_list.remove(tile3)
+            else:
+                return False
+        else:
+            # because sorted so if appears more than twice has to be part of triplet?
+            try:
+                test_list.remove(tile)
+                test_list.remove(tile)
+                test_list.remove(tile)
+                    
+            except Exception as e:
+                return False
+    return True
+
+
+
 def check_for_win_edited(tile_list):
-    tile_set =[]
-    for tile in tile_list:
-            if tile not in tile_set:
-                tile_set.append(tile)
 
     possible_pairs = []
-    for tile in tile_set:
+    for tile in tile_list:
         if tile_list.count(tile) >= 2:
             possible_pairs.append(tile)
 
@@ -96,30 +116,8 @@ def check_for_win_edited(tile_list):
         test_list = tile_list.copy()
         test_list.remove(pair)
         test_list.remove(pair)
-        # remaining is either part of sequence or part of triplet
-        while len(test_list) > 0:
-            tile = test_list[0]
-            if test_list.count(tile) < 3 and isinstance(tile.value, int):
-                tile2 = Tile(tile.suit_type, (tile.value) + 1)
-                tile3 = Tile(tile.suit_type, (tile.value) + 2)
-                if tile2 in test_list and tile3 in test_list:
-                    test_list.remove(tile)
-                    test_list.remove(tile2)
-                    test_list.remove(tile3)
-                else:
-                    return 
-            elif isinstance(tile.value, str):
-                if tile not in possible_pairs:
-                    return False
-                else:
-                    break
-            else:
-                # because sorted so if appears more than twice has to be part of triplet?
-                test_list.remove(tile)
-                test_list.remove(tile)
-                test_list.remove(tile)
+        if check_for_sets_of_three(test_list):
             return True
-
     return False
 # and isinstance(tile.value, int)
 try:
@@ -131,22 +129,20 @@ try:
         #             Tile("Dragon", "Z"),Tile("Dragon", "Z"),\
         #                 Tile("Wind", "N"),Tile("Wind", "N"),Tile("Wind", "N")]
         # print_tiles(special_case)
-        # print(check_for_win(special_case))
+        # print(check_for_win_edited(special_case))
         all_tiles = create_tiles()
-        test_cases = [[]]
-        for i in range(10):
-            for j in range(8):
-                index = random.randint(0,len(all_tiles)-1)
-                test_cases[i].append(all_tiles[index])
-                del all_tiles[index]
-            test_cases[i] = sort_tile_list(test_cases[i])
-            test_cases.append([])
-
-        del test_cases[-1]
-        for case in test_cases: 
-            if check_for_win(case):
-                print_tiles(case)
-                print(check_for_win(case))
+        test_case = []
+        # for i in range(15):
+        for j in range(14):
+            index = random.randint(0,len(all_tiles)-1)
+            test_case.append(all_tiles[index])
+            del all_tiles[index]
+        test_case = sort_tile_list(test_case)
+            # test_case.append([])
+        # del test_case[-1]
+        if check_for_win_edited(test_case):
+            print_tiles(test_case)
+            print(check_for_win_edited(test_case))
         
 except Exception as e:
     print(e)
